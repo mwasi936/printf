5e1af7ccc80c9893a22ca4cce6b6d7c8b8c634c5
odhiambovinogola <odhiambovinogola@gmail.com>
2023-10-17 22:37:14 +0300

    percent

---
+++ p_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * handle_percent - Handles the '%' character
+ *
+ * This function prints a '%' character and returns 1.
+ *
+ * Return: 1
+ */
+int handle_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
